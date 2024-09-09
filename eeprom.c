f7fd5b450d48aaf50b80be2587b8021849f2a7ed
joubiti <achraf.joubiti@usmba.ac.ma>
2024-09-09 20:05:05 +0100

    push API

---
+++ eeprom.c
@@ -0,0 +1,85 @@
+/**
+ * @file eeprom.h
+ * @author joubiti (github.com/joubiti)
+ * @brief AT24C256 EEPROM driver
+ * @version 0.1
+ * @date 2024-09-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+#include "eeprom.h"
+
+#define EEPROM_WRITE_ADDR        0xA0
+#define EEPROM_READ_ADDR         0xA1
+#define WRITE_CYCLE_TIME          5
+
+#define PAGE_SIZE       64
+#define PAGE_ADDRESS(x) (64 * x)
+
+
+void eeprom_write_bytes_to_page(eeprom_obj_t* obj, const uint8_t* data, uint8_t nb_of_bytes, uint8_t page_number){
+    uint8_t cmds[2+nb_of_bytes];
+    // send word address bytes (MSB first)
+    cmds[0] = (PAGE_ADDRESS(page_number) >> 8) & 0xFF;
+    cmds[1] = (PAGE_ADDRESS(page_number) & 0xFF);
+    for (uint8_t j = 0; j < nb_of_bytes; j ++){
+        cmds[j+2] = data[j];
+    }
+    obj->i2c_transfer_bytes(EEPROM_WRITE_ADDR, cmds, sizeof(cmds));
+    obj->delay_ms(WRITE_CYCLE_TIME);
+}
+
+
+void eeprom_read_bytes_from_page(eeprom_obj_t* obj, uint8_t* buf, uint8_t nb_of_bytes, uint8_t page_number){
+    uint8_t cmds[2];
+    // send word address bytes (MSB first)
+    cmds[0] = (PAGE_ADDRESS(page_number) >> 8) & 0xFF;
+    cmds[1] = (PAGE_ADDRESS(page_number) & 0xFF);
+    // dummy write
+    obj->i2c_transfer_bytes(EEPROM_WRITE_ADDR, cmds, sizeof(cmds));
+    // read bytes from EEPROM
+    obj->i2c_read_bytes(EEPROM_READ_ADDR, buf, nb_of_bytes);
+}
+
+void eeprom_write_bytes(eeprom_obj_t* obj, const uint8_t* data, uint8_t nb_of_bytes, uint16_t memory_address){
+    uint8_t cmds[2+nb_of_bytes];
+    cmds[0] = (memory_address >> 8) & 0xFF;
+    cmds[1] = (memory_address & 0xFF);
+    for (uint8_t j = 0; j < nb_of_bytes ; j ++){
+        cmds[j + 2] = data[j];
+    }
+    obj->i2c_transfer_bytes(EEPROM_WRITE_ADDR, cmds, sizeof(cmds));
+    obj->delay_ms(WRITE_CYCLE_TIME);
+}
+
+void eeprom_read_bytes(eeprom_obj_t* obj,  uint8_t* buf, uint8_t nb_of_bytes, uint16_t memory_address){
+    uint8_t cmds[2];
+    // send word address bytes (MSB first)
+    cmds[0] = (memory_address >> 8) & 0xFF;
+    cmds[1] = (memory_address & 0xFF);
+    // dummy write
+    obj->i2c_transfer_bytes(EEPROM_WRITE_ADDR, cmds, sizeof(cmds));
+    // read bytes from EEPROM
+    obj->i2c_read_bytes(EEPROM_READ_ADDR, buf, nb_of_bytes);
+}
+
+
+uint8_t eeprom_read_byte_from_last_address(eeprom_obj_t* obj){
+    uint8_t tmp;
+    obj->i2c_read_bytes(EEPROM_READ_ADDR, &tmp, 1);
+    return tmp;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ eeprom.h
@@ -0,0 +1,72 @@
+/**
+ * @file eeprom.h
+ * @author joubiti (github.com/joubiti)
+ * @brief AT24C256 EEPROM driver
+ * @version 0.1
+ * @date 2024-09-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+#include "stdint.h"
+
+/**
+ * @brief EEPROM object structure, contains function pointers to I2C low level functions and delay function
+ * 
+ */
+typedef struct{
+    void(*i2c_transfer_bytes)(uint16_t dev_addr, uint8_t* data, uint8_t nb_of_bytes);
+    void(*i2c_read_bytes)(uint16_t dev_addr, uint8_t* buf, uint8_t nb_of_bytes);
+    void(*delay_ms)(uint32_t t_ms);
+}eeprom_obj_t;
+
+/**
+ * @brief Writes a number of bytes sequentially from the starting address of an EEPROM page
+ * 
+ * @param obj: pointer to EEPROM object
+ * @param data: array of bytes
+ * @param nb_of_bytes: number of bytes
+ * @param page_number: index of page (512 pages in total, from 0 to 511) 
+ */
+void eeprom_write_bytes_to_page(eeprom_obj_t* obj, const uint8_t* data, uint8_t nb_of_bytes, uint8_t page_number);
+
+/**
+ * @brief Reads a number of bytes sequentially from the starting address of an EEPROM page
+ * 
+ * @param obj: pointer to EEPROM object 
+ * @param buf: buffer
+ * @param nb_of_bytes: number of bytes to read
+ * @param page_number: index of page (512 pages in total, from 0 to 511)
+ */
+void eeprom_read_bytes_from_page(eeprom_obj_t* obj, uint8_t* buf, uint8_t nb_of_bytes, uint8_t page_number);
+
+/**
+ * @brief Write a number of bytes starting from a memory address in EEPROM
+ * 
+ * @param obj: pointer to EEPROM object
+ * @param data: array of bytes
+ * @param nb_of_bytes: number of bytes
+ * @param memory_address: 16-bit memory address
+ */
+void eeprom_write_bytes(eeprom_obj_t* obj, const uint8_t* data, uint8_t nb_of_bytes, uint16_t memory_address);
+
+/**
+ * @brief Reads a number of bytes starting from a memory address in EEPROM
+ * 
+ * @param obj: pointer to EEPROM object
+ * @param buf: data buffer
+ * @param nb_of_bytes: number of bytes
+ * @param memory_address: 16-bit memory address
+ */
+void eeprom_read_bytes(eeprom_obj_t* obj,  uint8_t* buf, uint8_t nb_of_bytes, uint16_t memory_address);
+
+/**
+ * @brief Reads from last accessed EEPROM memory address
+ * @note To be used for POST tests
+ * 
+ * @param obj: pointer to EEPROM object
+ * @return uint8_t 
+ */
+uint8_t eeprom_read_byte_from_last_address(eeprom_obj_t* obj);
